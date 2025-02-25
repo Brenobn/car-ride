8f875a474126fb3f23840d6beb54445a450de319
Brenobn <brenobragabn@gmail.com>
2025-02-25 09:39:51 -0300

    Starting to use props as parameters and creating functions using navigate

--- src/screens/home/home.jsx
+++ src/screens/home/home.jsx
@@ -1,8 +1,16 @@
-import { Image, ImageBackground, Text, TouchableOpacity } from "react-native";
+import { Alert, Image, ImageBackground, Text, TouchableOpacity } from "react-native";
 import icons from "../../constants/icons";
 import { styles } from "./home.style";
 
-function Home() {
+function Home(props) {
+
+  function OpenPassenger() {
+    props.navigation.navigate("passenger");
+  }
+
+  function OpenRide() {
+    props.navigation.navigate("ride");
+  }
   return (
     <ImageBackground 
       source={icons.bg} 
@@ -15,22 +23,22 @@ function Home() {
       style={styles.logo} 
     />
 
-    <TouchableOpacity style={styles.btn}>
+    <TouchableOpacity style={styles.btn} onPress={OpenPassenger}>
       <Image 
         source={icons.passenger} 
         style={styles.img}
       />
       <Text style={styles.title}>Passageiro</Text>
-      <Text style={styles.text}>Econtre uma carona para você</Text>
+      <Text style={styles.text}>Econtre motoristas parceiros</Text>
     </TouchableOpacity>
 
-    <TouchableOpacity style={styles.btn}>
+    <TouchableOpacity style={styles.btn} onPress={OpenRide}>
       <Image 
         source={icons.driver} 
         style={styles.img}
       />
       <Text style={styles.title}>Motorista</Text>
-      <Text style={styles.text}>Ofereça carona em seu carro</Text>
+      <Text style={styles.text}>Aceite corridas</Text>
     </TouchableOpacity>
 
     </ImageBackground>
