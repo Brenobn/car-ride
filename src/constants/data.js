a46e33f3857d5c3f2688d46b0429dd36e16d35bc
Brenobn <brenobragabn@gmail.com>
2025-02-25 09:32:35 -0300

    Creating data.js file with data on supposed routes and users of the app

---
+++ src/constants/data.js
@@ -0,0 +1,58 @@
+export const json_ride = {
+  ride_id: 1,
+  passenger_user_id: 1,
+  passenger_name: "Breno Braga",
+  passenger_phone: "(21) 98047-9073",
+  pickup_address: "Rua Heráclito Graça, 145 - Lins de Vasconselos",
+  pickup_date: "20725-080",
+  dropoff_address: "Shopping Nova América",
+  status: "A",
+  driver_user_id: 2,
+  driver_name: "João Martins",
+  latitude: "-22.903622",
+  longitude: "-43.279308"
+}
+
+export const json_rides = [{
+  ride_id: 1,
+  passenger_user_id: 1,
+  passenger_name: "Breno Braga",
+  passenger_phone: "(21) 98047-9073",
+  pickup_address: "Rua Heráclito Graça, 145 - Lins de Vasconselos",
+  pickup_date: "20725-080",
+  dropoff_address: "Shopping Nova América",
+  status: "P",
+  driver_user_id: 2,
+  driver_name: "João Martins",
+  latitude: "-22.903622",
+  longitude: "-43.279308"
+},
+{
+  ride_id: 2,
+  passenger_user_id: 3,
+  passenger_name: "Lucas Neto",
+  passenger_phone: "(21) 99999-9999",
+  pickup_address: "Rua Ernestina, 46 - Lins de Vasconselos",
+  pickup_date: "20710-000",
+  dropoff_address: "Shopping 45",
+  status: "A",
+  driver_user_id: 4,
+  driver_name: "Vitor Brandão",
+  latitude: "-22.911393",
+  longitude: "-43.283748"
+},
+{
+  ride_id: 3,
+  passenger_user_id: 2,
+  passenger_name: "Dáfine Lira",
+  passenger_phone: "(21) 98999-9073",
+  pickup_address: "Rua Xavier da silveira, 42 - Copacabana",
+  pickup_date: "22061-010",
+  dropoff_address: "Praça Saenz Peña",
+  status: "F",
+  driver_user_id: 1,
+  driver_name: "Alicia Keys",
+  latitude: "-22.977065",
+  longitude: "-43.189748"
+},
+]
