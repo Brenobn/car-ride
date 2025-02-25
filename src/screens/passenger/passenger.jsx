072d1e4902682db4f820979edf99148e36dc024c
Brenobn <brenobragabn@gmail.com>
2025-02-25 07:42:37 -0300

    Importing View, MapView, useState and starting to use imports

--- src/screens/passenger/passenger.jsx
+++ src/screens/passenger/passenger.jsx
@@ -1,11 +1,36 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import MyButton from "../../components/myButton/myButton.jsx";
-import MapView from "react-native-maps";
+import MapView, { Marker ,PROVIDER_DEFAULT } from "react-native-maps";
 import { styles } from "./passenger.style.js";
+import { useState } from "react";
 
 function Passenger(props) {
+  
+  const [myLocation, setMyLocation] = useState({
+    latitude: 20,
+    longitude: 20
+  });
+
   return(
     <View style={styles.container}>
+      <MapView 
+        style={styles.map} 
+        provider={PROVIDER_DEFAULT}
+        initialRegion={{
+          latitude: -22.911266,
+          longitude: -43.282739,
+          latitudeDelta: 0.004,
+          longitudeDelta: 0.004
+        }}
+      >
+      <Marker coordinate={{
+        latitude: -22.911266,
+        longitude: -43.282739
+      }}
+        title="Breninn"
+        description="Rua Heráclito Graça 145"
+      />
+      </MapView>
       <MyButton text="TESTE"/>
     </View>
   );
