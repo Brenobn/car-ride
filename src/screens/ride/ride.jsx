53cdaf30f1fbc05afe87cf7c28d3fcb3c5cdba63
Brenobn <brenobragabn@gmail.com>
2025-03-14 08:39:11 -0300

    Importing api and using it in the RequestRides function. Handling errors with try catch. Making conditional in the JSX structure

--- src/screens/ride/ride.jsx
+++ src/screens/ride/ride.jsx
@@ -1,12 +1,12 @@
 import { FlatList, Text, TouchableOpacity, View, Image } from "react-native";
 import { styles } from "./ride.style.js";
-import { json_rides } from "../../constants/data.js";
 import icons from "../../constants/icons.js";
 import { useEffect, useState } from "react";
+import { api, HandleError } from "../../constants/api.js";
 
 function Ride(props) {
 
-  const userId = 2; 
+  const userId = 5; 
   const [rides, setRides] = useState([]);
 
   function ClickRide(id) {
@@ -17,9 +17,18 @@ function Ride(props) {
   }
 
   async function RequestRides(params) {
-    // Acessar a API em busca das corridas...
+
+    try {
+      const response = await api.get("/rides/drivers/" + userId);
+
+      if (response.data) {
+        setRides(response.data);
+      }
+
+    } catch (error) {
+      HandleError(error);
+    }
     
-    setRides(json_rides);
   }
 
   useEffect(() => {
@@ -36,7 +45,10 @@ function Ride(props) {
           return(
             <TouchableOpacity style={styles.RideContainer} onPress={() => ClickRide(item.ride_id)}>
               <View style={styles.nameContainer}>
-                <Image source={icons.car} style={styles.car}/>
+                {
+                  item.driver_user_id == userId && <Image source={icons.car} style={styles.car}/>
+                }
+                
                 <Text style={styles.name}>{item.passenger_name}</Text>
               </View>
               <Text style={styles.address}>Origem: {item.pickup_address}</Text>
